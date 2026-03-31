3a55fdd06b8a6ee31fb7201c1edd4d466a896c47
Project Contributor <contributor@example.com>
2026-03-31 15:51:47 +0530

    chore: migrate from gsap-trial to official gsap package

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      gsap: "gsap-trial",
-    },
-  },
   optimizeDeps: {
     include: ["@react-three/drei", "@react-three/fiber", "three"],
   },
